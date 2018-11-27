71f44fe8abdd80b0d1b539f6b1bfecfa0e83904e
sung <s.kim.castru@gmail.com>
2018-11-27 20:59:38 +0200

    Creating new pages for redirected and routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Movies from "./components/movies";
+import Coustumers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="container-fluid mt-4 ">
-          <Movies />
-        </div>
+        <main className="container-fluid mt-4 ">
+          <Switch>
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Coustumers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
       </div>
     );
   }
---
+++ src/components/customers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Coustumers = () => {
+  return <h1>Costumers </h1>;
+};
+
+export default Coustumers;
