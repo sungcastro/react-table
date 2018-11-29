ad1e17c388c0e00fdd60db4432862cd0b7476ed2
sung <s.kim.castru@gmail.com>
2018-11-29 20:21:45 +0200

    Linking to the movie form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Coustumers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <NavBar />
         <main className="container-fluid mt-4 ">
           <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Coustumers} />
             <Route path="/rentals" component={Rentals} />
