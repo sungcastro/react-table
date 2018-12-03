4dfd5b5963e0b720f4fb141928bde0d31fc39f94
sung <s.kim.castru@gmail.com>
2018-12-03 20:22:02 +0200

    Basic validation implementation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -11,15 +11,22 @@ class LoginForm extends Component {
   };
 
   validate = () => {
-    return {
-      username: "Username is required"
-    };
+    const errors = {};
+
+    const { username, password } = this.state.account;
+
+    if (username.trim() === "") errors.username = "Username is required";
+
+    if (password.trim() === "") errors.password = "Password is required";
+
+    return Object.keys(errors).length === 0 ? null : errors;
   };
 
   handleSubmit = e => {
     e.preventDefault();
 
     const errors = this.validate();
+    console.log(errors);
     this.setState({ errors });
     if (errors) return;
 
