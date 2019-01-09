4ee5e910731aea73d22a72fe8a864094c7fcd649
sung <s.kim.castru@gmail.com>
2019-01-09 20:34:11 +0200

    Register Form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
         <NavBar />
         <main className="container-fluid mt-4 ">
           <Switch>
+            <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
---
+++ src/components/registerForm.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Form from "./common/form";
+import Joi from "joi-browser";
+
+class RegisterForm extends Form {
+  state = {
+    data: {
+      username: "",
+      password: "",
+      name: ""
+    },
+    errors: {}
+  };
+
+  schema = {
+    username: Joi.string()
+      .required()
+      .email()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .min(5)
+      .label("Password"),
+    name: Joi.string()
+      .required()
+      .label("Name")
+  };
+
+  doSubmit = () => {
+    console.log("Submitted");
+  };
+
+  render() {
+    return (
+      <div className="card container w-25 mt-5 p-5">
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RegisterForm;
