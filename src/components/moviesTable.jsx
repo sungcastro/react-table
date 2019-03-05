9a2583e7303a1252239a0246390bc54cbe6a445e
sung <s.kim.castru@gmail.com>
2019-03-05 20:12:00 +0200

    Hiding the Delete Column for not admins

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import auth from "../services/authService";
 import Table from "./common/table";
 import Like from "./common/like";
 import { Link } from "react-router-dom";
@@ -21,19 +22,27 @@ class MoviesTable extends Component {
           onToggleHeart={() => this.props.onLike(movie)}
         />
       )
-    },
-    {
-      key: "delete",
-      content: movie => (
-        <button
-          onClick={() => this.props.onDelete(movie)}
-          className="btn btn-warning text-white"
-        >
-          Delete
-        </button>
-      )
     }
   ];
+
+  deleteColumn = {
+    key: "delete",
+    content: movie => (
+      <button
+        onClick={() => this.props.onDelete(movie)}
+        className="btn btn-warning text-white"
+      >
+        Delete
+      </button>
+    )
+  };
+
+  constructor() {
+    super();
+
+    const user = auth.getCurrentUser();
+    if (user && user.isAdmin) this.columns.push(this.deleteColumn);
+  }
 
   render() {
     const { movies, sortColum, onSorting } = this.props;
